c29b276cb6240d83533e8854af73778da3817edd
Felipe Alfaya <felipealfaya90@gmail.com>
2022-07-12 12:49:53 -0300

    sixth commit, adding the routerlink to new pages.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,11 +5,19 @@ import { PagesRoutingModule } from './pages.routing.module';
 
 //pages
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    ProjetosComponent,
+    ContatoComponent,
+  ],
 
   imports: [CommonModule, RouterModule, PagesRoutingModule],
-  exports: [HomeComponent],
+  exports: [HomeComponent, AboutComponent, ProjetosComponent, ContatoComponent],
 })
 export class PagesModule {}
--- src/app/pages/pages.routing.module.ts
+++ src/app/pages/pages.routing.module.ts
@@ -3,11 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 
 //pages
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'projetos',
+    component: ProjetosComponent,
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent,
   },
 ];
 
